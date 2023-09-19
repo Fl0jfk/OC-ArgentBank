a383208ebb765b6314cb5ebadc4f59619bb3beb2
Fl0jfk <florian.hacqueville@hotmail.fr>
2023-09-19 15:56:35 +0200

    so many fixs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,14 +2,14 @@ import argentBankLogo from '../../assets/img/argentBankLogo.png';
 import './header.scss';
 import '../../assets/fontAwesome/fontawesome-free-6.4.2-web/css/all.min.css';
 import { Link } from 'react-router-dom';
-import { authOutSuccess } from "../../redux/Slices/authSlice";
+import { authOut } from "../../redux/Slices/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function Header() {
     const dispatch = useDispatch();        
     const connectedOrNot = useSelector((state) => state.auth.isAuth);
     const handleLogOut = () => {
-        dispatch(authOutSuccess());
+        dispatch(authOut());
     };
     return (
         <nav className="main-nav">
@@ -23,10 +23,13 @@ function Header() {
                 </Link>
                 <div>
                     {  connectedOrNot ? 
-                        <Link to="/sign" className="link_SignOut" onClick={handleLogOut}>
-                        <i className="fa-solid fa-right-from-bracket"></i>
-                            Sign Out
-                        </Link>
+                        <>
+                            <Link to="/user">User Space</Link>
+                            <Link to="/sign" className="link_SignOut" onClick={handleLogOut}>
+                                <i className="fa-solid fa-right-from-bracket"></i>
+                                Sign Out
+                            </Link>
+                        </>
                         :
                         <Link className="main-nav-item" to="/sign">
                             <i className="fa fa-user-circle"></i>
